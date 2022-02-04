8b46396698c8b168c4423410be0f7ab864c3e244
Thomas Jakobi <thomas.jakobi@partout.info>
2022-02-04 16:39:46 +0100

    Update lexicon for 2.0

--- core/components/colorpicker/lexicon/de/default.inc.php
+++ core/components/colorpicker/lexicon/de/default.inc.php
@@ -5,14 +5,13 @@
  * @package colorpicker
  * @subpackage lexicon
  */
-
 $_lang['colorpicker'] = 'ColorPicker';
 $_lang['colorpicker.format'] = 'Format';
 $_lang['colorpicker.format_desc'] = 'CSS-Farbformat';
 $_lang['colorpicker.format_hex'] = 'Hexadezimal';
-$_lang['colorpicker.format_rgb'] = 'RGB';
 $_lang['colorpicker.format_hsl'] = 'HSL';
+$_lang['colorpicker.format_rgb'] = 'RGB';
 $_lang['colorpicker.output'] = 'Ausgabe';
-$_lang['colorpicker.output_desc'] = 'Beispiel für eine CSS-Ausgabe => #FFFFFF oder rgb (255,255,255) oder hsl (1,1,1). Beispiel für eine JSON-Ausgabe => {"FF", "FF", "FF"} oder {255,255,255} oder {1,1,1} ';
 $_lang['colorpicker.output_css'] = 'CSS';
+$_lang['colorpicker.output_desc'] = 'Beispiel für eine CSS-Ausgabe => #FFFFFF oder rgb (255,255,255) oder hsl (1,1,1). Beispiel für eine JSON-Ausgabe => {"FF", "FF", "FF"} oder {255,255,255} oder {1,1,1} ';
 $_lang['colorpicker.output_json'] = 'JSON';
