c7c35d912ef593465e1a1cb9726e321863cad8a4
Thomas Jakobi <thomas.jakobi@partout.info>
2022-12-31 01:30:25 +0100

    Normalize build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ pkg.dependencies.forEach(function (dependency, index) {
     }
 });
 
-gulp.task('scripts-mgr', function () {
+const scriptsMgr = function () {
     return gulp.src([
         'source/js/mgr/colorpicker.js',
         'node_modules/@melloware/coloris/dist/coloris.js'
@@ -43,9 +43,10 @@ gulp.task('scripts-mgr', function () {
         .pipe(uglify())
         .pipe(header(banner + '\n', {pkg: pkg}))
         .pipe(gulp.dest('assets/components/colorpicker/js/mgr/'))
-});
+};
+gulp.task('scripts', gulp.series(scriptsMgr));
 
-gulp.task('sass-mgr', function () {
+const sassMgr = function () {
     return gulp.src([
         'source/sass/mgr/colorpicker.scss'
     ])
@@ -69,61 +70,63 @@ gulp.task('sass-mgr', function () {
         }))
         .pipe(footer('\n' + banner, {pkg: pkg}))
         .pipe(gulp.dest('assets/components/colorpicker/css/mgr/'))
-});
+};
+gulp.task('sass', gulp.series(sassMgr));
 
-gulp.task('images-mgr', function () {
+const imagesMgr = function () {
     return gulp.src('./source/img/**/*.+(png|jpg|gif|svg)')
         .pipe(gulp.dest('assets/components/colorpicker/img/'));
-});
+};
+gulp.task('images', gulp.series(imagesMgr));
 
-gulp.task('bump-copyright', function () {
+const bumpCopyright = function () {
     return gulp.src([
         'core/components/colorpicker/model/colorpicker/colorpicker.class.php',
         'core/components/colorpicker/src/ColorPicker.php'
     ], {base: './'})
         .pipe(replace(/Copyright 2017(-\d{4})? by/g, 'Copyright ' + (year > 2017 ? '2017-' : '') + year + ' by'))
         .pipe(gulp.dest('.'));
-});
-gulp.task('bump-version', function () {
+};
+const bumpVersion = function () {
     return gulp.src([
         'core/components/colorpicker/src/ColorPicker.php'
     ], {base: './'})
         .pipe(replace(/version = '\d+\.\d+\.\d+[-a-z0-9]*'/ig, 'version = \'' + pkg.version + '\''))
         .pipe(gulp.dest('.'));
-});
-gulp.task('bump-options', function () {
+};
+const bumpOptions = function () {
     return gulp.src([
         'core/components/colorpicker/elements/tv/input/tpl/colorpicker.options.tpl',
         'core/components/colorpicker/elements/tv/output/tpl/colorpicker.options.tpl'
     ], {base: './'})
         .pipe(replace(/&copy; 2017(-\d{4})?/g, '&copy; ' + (year > 2017 ? '2017-' : '') + year))
         .pipe(gulp.dest('.'));
-});
-gulp.task('bump-docs', function () {
+};
+const bumpDocs = function () {
     return gulp.src([
         'mkdocs.yml',
     ], {base: './'})
         .pipe(replace(/&copy; 2017(-\d{4})?/g, '&copy; ' + (year > 2017 ? '2017-' : '') + year))
         .pipe(gulp.dest('.'));
-});
-gulp.task('bump-requirements', function () {
+};
+const bumpRequirements = function () {
     return gulp.src([
         'docs/index.md',
     ], {base: './'})
         .pipe(replace(/[*-] MODX Revolution \d.\d.*/g, '* MODX Revolution ' + modxversion + '+'))
         .pipe(replace(/[*-] PHP (v)?\d.\d.*/g, '* PHP ' + phpversion + '+'))
         .pipe(gulp.dest('.'));
-});
-gulp.task('bump', gulp.series('bump-copyright', 'bump-version', 'bump-options', 'bump-docs', 'bump-requirements'));
+};
+gulp.task('bump', gulp.series(bumpCopyright, bumpVersion, bumpOptions, bumpDocs, bumpRequirements));
 
 gulp.task('watch', function () {
     // Watch .js files
-    gulp.watch(['source/js/**/*.js'], gulp.series('scripts-mgr'));
+    gulp.watch(['./source/js/**/*.js'], gulp.series('scripts'));
     // Watch .scss files
-    gulp.watch(['source/sass/**/*.scss'], gulp.series('sass-mgr'));
+    gulp.watch(['./source/sass/**/*.scss'], gulp.series('sass'));
     // Watch *.(png|jpg|gif|svg) files
-    gulp.watch(['source/img/**/*.(png|jpg|gif|svg)'], gulp.series('images-mgr'));
+    gulp.watch(['./source/img/**/*.(png|jpg|gif|svg)'], gulp.series('images'));
 });
 
 // Default Task
-gulp.task('default', gulp.series('bump', 'scripts-mgr', 'sass-mgr', 'images-mgr'));
+gulp.task('default', gulp.series('bump', 'scripts', 'sass', 'images'));
