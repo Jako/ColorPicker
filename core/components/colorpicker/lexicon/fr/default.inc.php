52cb363f0058e56986235cd52aa573c025bebbcd
Thomas Jakobi <thomas.jakobi@partout.info>
2026-06-30 10:17:24 +0200

    Normalize space sign before two-part punctation marks

--- core/components/colorpicker/lexicon/fr/default.inc.php
+++ core/components/colorpicker/lexicon/fr/default.inc.php
@@ -25,6 +25,6 @@ $_lang['colorpicker.output_type_css'] = 'CSS';
 $_lang['colorpicker.output_type_desc'] = 'Les exemples de sortie CSS sont décrits dans la section Format. Exemple de sortie JSON : Hex : ["80","ff","00"] ou ["80","ff","00",0.5], RGB : [128,255,0] ou [128,255,0,0.5], HSL : [90,"100%","50%"] ou [90,"100%","50%",0.5]';
 $_lang['colorpicker.output_type_json'] = 'JSON';
 $_lang['colorpicker.swatches'] = 'Échantillons';
-$_lang['colorpicker.swatches_desc'] = 'Un tableau codé JSON des échantillons de couleurs souhaités à afficher. Si la liste est vide, les échantillons sont désactivés. Exemple : ["#264653", "#2a9d8f", "#e9c46a", "rgb(244,162,97)", "#e76f51", "#d62828", "marine", "#07b", "#0096c7" , "#00b4d880", "rgba(0,119,182,0.8)"]';
+$_lang['colorpicker.swatches_desc'] = 'Un tableau codé JSON des échantillons de couleurs souhaités à afficher. Si la liste est vide, les échantillons sont désactivés. Exemple : ["#264653", "#2a9d8f", "#e9c46a", "rgb(244,162,97)", "#e76f51", "#d62828", "marine", "#07b", "#0096c7" , "#00b4d880", "rgba(0,119,182,0.8)"]';
 $_lang['colorpicker.swatchesOnly'] = 'Échantillons seulement';
 $_lang['colorpicker.swatchesOnly_desc'] = 'Mettez cette valeur à true pour masquer tous les widgets de sélection de couleur (spectre, teinte, …) à l’exception des échantillons de couleur.';
